a2cf35aedb77f0e84dc74c817c5eed1ea1558f24
sazzadsazib <sazib66@gmail.com>
2019-03-23 03:30:24 +0600

    fix: work in progress

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+     trailingComma: "es5",
+     tabWidth: 5,
+     semi: true,
+     singleQuote: false,
+     printWidth: 140,
+     jsxSingleQuote: true,
+     bracketSpacing: true,
+     jsxBracketSameLine: true,
+     arrowParens: "always",
+};
